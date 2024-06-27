8df39c6c2ece676092789ded074f4ae35ef0582d
nikkyeya-jsm <nikky@jsmastery.pro>
2024-06-27 15:33:10 +0800

    fix: title

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,6 @@ import { SignIn } from '@clerk/nextjs';
 export default function Page() {
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center gap-10">
-      <h2 className="text-2xl font-bold">Sign-in</h2>
       <SignIn />
     </main>
   );
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,6 @@ import { SignUp } from '@clerk/nextjs';
 export default function Page() {
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center gap-10">
-      <h2 className="text-2xl font-bold">Sign-up</h2>
       <SignUp />
     </main>
   );
