a7ca93dd95daa69f43fb7605ecefb618b44a27b8
nikkyeya-jsm <nikky@jsmastery.pro>
2024-07-09 21:14:28 +0800

    fix: add override to text mention notifications

--- components/Notifications.tsx
+++ components/Notifications.tsx
@@ -4,8 +4,13 @@ import {
   useInboxNotifications,
   useUnreadInboxNotificationsCount,
 } from '@liveblocks/react/suspense';
-import { InboxNotification, InboxNotificationList } from '@liveblocks/react-ui';
+import {
+  InboxNotification,
+  InboxNotificationList,
+  LiveblocksUIConfig,
+} from '@liveblocks/react-ui';
 import Image from 'next/image';
+import { ReactNode } from 'react';
 
 import {
   Popover,
@@ -16,8 +21,6 @@ import {
 export const Notifications = () => {
   const { inboxNotifications } = useInboxNotifications();
   const { count } = useUnreadInboxNotificationsCount();
-
-  console.log({ count });
 
   const unreadNotifications = inboxNotifications.filter(
     (notification) => !notification.readAt, // Filter unread notifications
@@ -40,63 +43,73 @@ export const Notifications = () => {
         align="end"
         className="w-[460px] border-none bg-dark-200 shadow-lg"
       >
-        <InboxNotificationList>
-          {unreadNotifications.length <= 0 && (
-            <p className="py-2 text-center text-dark-500">
-              No notifications yet
-            </p>
-          )}
+        <LiveblocksUIConfig
+          overrides={{
+            INBOX_NOTIFICATION_TEXT_MENTION: (user: ReactNode) => {
+              return <>{user} mentioned you</>;
+            },
+          }}
+        >
+          <InboxNotificationList>
+            {unreadNotifications.length <= 0 && (
+              <p className="py-2 text-center text-dark-500">
+                No notifications yet
+              </p>
+            )}
 
-          {unreadNotifications.length > 0 &&
-            unreadNotifications.map((inboxNotification) => (
-              <InboxNotification
-                key={inboxNotification.id}
-                inboxNotification={inboxNotification}
-                className="bg-dark-200 text-white"
-                href={`/documents/${inboxNotification.roomId}`}
-                showActions={false}
-                kinds={{
-                  thread: (props) => (
-                    <InboxNotification.Thread
-                      {...props}
-                      showRoomName={false}
-                      showActions={false}
-                    />
-                  ),
-                  textMention: (props) => (
-                    <InboxNotification.TextMention
-                      {...props}
-                      showRoomName={false}
-                    />
-                  ),
-                  $documentAccess: (props) => {
-                    const { title, avatar } =
-                      props.inboxNotification.activities[0].data;
-
-                    return (
-                      <InboxNotification.Custom
+            {unreadNotifications.length > 0 &&
+              unreadNotifications.map((inboxNotification: any) => (
+                <InboxNotification
+                  key={inboxNotification.id}
+                  inboxNotification={inboxNotification}
+                  className="bg-dark-200 text-white"
+                  href={`/documents/${inboxNotification.roomId}`}
+                  showActions={false}
+                  kinds={{
+                    thread: (props) => (
+                      <InboxNotification.Thread
                         {...props}
-                        title={title}
-                        aside={
-                          <InboxNotification.Icon className="bg-transparent">
-                            <Image
-                              src={(avatar as string) || ''}
-                              width={36}
-                              height={36}
-                              alt="avatar"
-                              className="rounded-full"
-                            />
-                          </InboxNotification.Icon>
-                        }
-                      >
-                        {props.children}
-                      </InboxNotification.Custom>
-                    );
-                  },
-                }}
-              />
-            ))}
-        </InboxNotificationList>
+                        showRoomName={false}
+                        showActions={false}
+                      />
+                    ),
+                    textMention: (props) => {
+                      return (
+                        <InboxNotification.TextMention
+                          {...props}
+                          showRoomName={false}
+                        />
+                      );
+                    },
+                    $documentAccess: (props) => {
+                      const { title, avatar } =
+                        props.inboxNotification.activities[0].data;
+
+                      return (
+                        <InboxNotification.Custom
+                          {...props}
+                          title={title}
+                          aside={
+                            <InboxNotification.Icon className="bg-transparent">
+                              <Image
+                                src={(avatar as string) || ''}
+                                width={36}
+                                height={36}
+                                alt="avatar"
+                                className="rounded-full"
+                              />
+                            </InboxNotification.Icon>
+                          }
+                        >
+                          {props.children}
+                        </InboxNotification.Custom>
+                      );
+                    },
+                  }}
+                />
+              ))}
+          </InboxNotificationList>
+        </LiveblocksUIConfig>
       </PopoverContent>
     </Popover>
   );
